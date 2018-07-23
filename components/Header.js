6acce80afc55a4f66c02af95f227d05d43a2091e
duongtdn <duongtdn@gmail.com>
2018-07-23 13:08:16 +0000

    implement header

--- components/Header.js
+++ components/Header.js
@@ -11,6 +11,16 @@ class Header extends Component {
     return (
       <div className="w3-container w3-bar">
         <div className="w3-bar-item"> <h2> Catalog </h2> </div>
+
+        <div className="w3-bar-item w3-right w3-hide-medium w3-hide-large"> 
+          <i className="fa fa-sign-in"  ariaHidden="true" />
+        </div>
+
+        <div className="w3-bar-item w3-right w3-hide-small" style={{marginTop: '16px'}}> 
+          <button className="w3-button w3-hover-none w3-hover-text-blue"> Sign up </button>
+          <button className="w3-button w3-large w3-blue w3-card-4 w3-round"> Login </button>
+        </div>
+
       </div>
     )
   }
