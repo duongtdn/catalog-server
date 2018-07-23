3b5830641557070181a5f472ace8f0beef65bba5
duongtdn <duongtdn@gmail.com>
2018-07-23 23:32:04 +0000

    start coding sidebar

--- components/Header.js
+++ components/Header.js
@@ -2,9 +2,30 @@
 
 import React, { Component } from 'react'
 
+class Sidebar extends Component {
+  constructor(props) {
+    super(props)
+  }
+  render() {
+    return (
+      <div className="w3-sidebar w3-bar-block w3-hide-medium w3-hide-large" style={{right:0}}>
+        <div className="w3-bar-item" > 
+          <button className="w3-button w3-block w3-large w3-blue w3-card-4 w3-round"> Login </button>
+          <button className="w3-button w3-block w3-hover-none w3-hover-text-blue no-outline"> Sign up </button>
+        </div>
+        <hr />
+        <a href="#" className="w3-bar-item w3-button">Home</a>
+        <a href="#" className="w3-bar-item w3-button">About</a>
+        <a href="#" className="w3-bar-item w3-button">Contact</a>
+      </div>
+    )
+    
+  }
+}
+
 class Header extends Component {
   constructor(props) {
-    super (props)
+    super(props)
   }
 
   render() {
@@ -16,15 +37,17 @@ class Header extends Component {
           <i className="fa fa-bars w3-xlarge"  aria-hidden="true" ></i>
         </div>
 
+        <Sidebar />
+
         <div className="w3-bar-item w3-right w3-hide-small" style={{marginTop: '16px'}}> 
           <button className="w3-button w3-hover-none w3-hover-text-blue no-outline"> Sign up </button>
           <button className="w3-button w3-large w3-blue w3-card-4 w3-round"> Login </button>
         </div>
 
         <div className="w3-bar-item w3-right w3-hide-small w3-border-right" style={{marginTop: '16px'}}> 
-          <button className="w3-button no-outline"> Home </button>
-          <button className="w3-button no-outline"> About </button>
-          <button className="w3-button no-outline"> Contact </button>
+          <a href="#" className="w3-bar-item w3-button no-outline">Home</a>
+          <a href="#" className="w3-bar-item w3-button no-outline">About</a>
+          <a href="#" className="w3-bar-item w3-button no-outline">Contact</a>
         </div>
 
       </div>
