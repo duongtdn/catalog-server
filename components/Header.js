5eefa8417fca10e00d3999b0b228729eb8066563
duongtdn <duongtdn@gmail.com>
2018-07-24 15:03:48 +0000

    add api to authen

--- components/Header.js
+++ components/Header.js
@@ -5,6 +5,12 @@ import React, { Component } from 'react'
 import { logout } from '@stormgle/auth-client'
 import { Login, bindUserProvider  } from '@stormgle/react-user'
 
+const _api = {
+  login: process.env.API_LOGIN || 'http://localhost:3100/auth/login',
+  signup: process.env.API_SIGNUP || 'http://localhost:3100/auth/signup',
+  check: process.env.API_CHECK || 'http://localhost:3100/check/user',
+}
+
 class Sidebar extends Component {
   constructor(props) {
     super(props);
@@ -43,7 +49,10 @@ class LoginPanel extends Component {
             style= {{ display: _display, right: 0, width: '100%', background: 'linear-gradient(to bottom right, #f1f1f1  0%, #ddffff  100%)' }} 
       > 
         <div style={{margin: 'auto'}} >
-          <Login show = {this.props.display} close = {this.props.close} /> 
+          <Login show = {this.props.display} 
+                 close = {this.props.close} 
+                 api = {_api}
+          /> 
         </div>
       </div>
     )
