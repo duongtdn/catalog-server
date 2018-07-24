9bf951d34e7252a1e97c5fae5e35a558d7ad2d37
duongtdn <duongtdn@gmail.com>
2018-07-24 02:12:47 +0000

    change color of sign-up button to orange

--- components/Header.js
+++ components/Header.js
@@ -17,7 +17,7 @@ class Sidebar extends Component {
 
         <div className="w3-bar-item w3-border-bottom" style={{marginTop: '48px'}} > 
           <button className="w3-button w3-block w3-large w3-blue w3-card-4 w3-round"> Login </button>
-          <button className="w3-button w3-block w3-hover-none w3-hover-text-blue no-outline"> Sign up </button>
+          <button className="w3-button w3-block w3-text-orange w3-hover-none w3-hover-text-blue no-outline"> Sign up </button>
         </div>
         
         <a href="#" className="w3-bar-item w3-button">Home</a>
@@ -54,7 +54,7 @@ class Header extends Component {
         <Sidebar display = {this.state.sidebar} close = {this.closeSidebar} />
 
         <div className="w3-bar-item w3-right w3-hide-small" style={{marginTop: '16px'}}> 
-          <button className="w3-button w3-hover-none w3-hover-text-blue no-outline"> Sign up </button>
+          <button className="w3-button w3-text-orange w3-hover-none w3-hover-text-blue no-outline"> Sign up </button>
           <button className="w3-button w3-large w3-blue w3-card-4 w3-round"> Login </button>
         </div>
 
