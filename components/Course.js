d93e2ff130e3659bfe4b0aad3d247490489b8d73
duongtdn <duongtdn@gmail.com>
2018-07-30 10:04:10 +0000

    implementing course

--- components/Course.js
+++ components/Course.js
@@ -21,16 +21,45 @@ class Course extends Component {
           <h2> {course.title} </h2>
           
           {/* render 1st panel: course information */}
-          <div className="w3-bar">
-            {/* left side */}
-            <div className="w3-bar-item">
-              lefy side
+          <div className="w3-row">
+
+            {/* left */}
+            <div className="w3-half">
+
+              <p className="w3-text-grey" style={{fontStyle: 'italic'}} > {course.snippet} </p>
+
+              <p style={{fontWeight: 'bold'}}> <span className="w3-text-grey">Level: </span> <span  className="w3-text-green" > {course.level} </span> </p>
+
+              <p className="w3-text-grey" style={{fontWeight: 'bold'}}> Skills </p>
+              {
+                course.skills.map(skill => (
+                  <p key={skill}  className="cursor-pointer w3-text-blue" style={{paddingLeft: '16px', fontWeight: 'bold'}} > + {skill} </p>
+                ))
+              }
+
+              <p className="w3-text-grey" style={{fontWeight: 'bold'}}> Required for Certificates </p>
+              {
+                course.certificates.map(cert => (
+                  <p key={cert}  className="cursor-pointer w3-text-blue" style={{paddingLeft: '16px', fontWeight: 'bold'}} > + {cert} </p>
+                ))
+              }
+
+              <hr />
+
+              <div>
+                <button className="w3-button w3-green w3-card-4"> Enroll Now </button>
+              </div>
+
             </div>
-            {/* right side */}
-            <div className="w3-bar-item">
-              right side
+            
+            {/* right */}
+            <div className="w3-half">
+              <div className="embed-responsive">
+                <iframe src="https://www.youtube.com/embed/tgbNymZ7vqY" />
+              </div>
             </div>
-          </div>
+
+          </div> 
 
         </div>
 
