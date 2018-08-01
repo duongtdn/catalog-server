1d37554d9d4e1e282bb923f052d5886873a47469
duongtdn <duongtdn@gmail.com>
2018-08-01 12:18:15 +0000

    move localeString to lib

--- components/Course.js
+++ components/Course.js
@@ -4,21 +4,7 @@ import React, { Component } from 'react'
 import { bindUserProvider  } from '@stormgle/react-user'
 
 import Header from './Header'
-
-function localeString(x, sep, grp) {
-  var sx = (''+x).split('.'), s = '', i, j;
-  sep || (sep = ' '); // default seperator
-  grp || grp === 0 || (grp = 3); // default grouping
-  i = sx[0].length;
-  while (i > grp) {
-      j = i - grp;
-      s = sep + sx[0].slice(j, i) + s;
-      i = j;
-  }
-  s = sx[0].slice(0, i) + s;
-  sx[0] = s;
-  return sx.join('.');
-}
+import { localeString } from '../lib/utils'
 
 class Course extends Component {
   constructor(props) {
